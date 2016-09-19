3529e39ac2c352c2b8d4b3a220109ccf8be6f8fe
iclosure <iclosure@163.com>
2016-09-19 10:44:23 +0800

    增加visual studio for Qt5 工程向导工程

---
+++ tools/wizards/visualstudio/qt5/JFramework Wizards/ComponentProject/Templates/1033/componentui.cpp
@@ -0,0 +1,74 @@
+﻿#include "precomp.h"
+#include "%{UiHdrFileName}"
+%{JS: Cpp.openNamespaces('%{UiClass}')}
+@if '%{IncludeQSharedData}'
+class %{UiClassName}Data : public QSharedData
+{
+    friend class %{UiClassName};
+public:
+    explicit %{UiClassName}Data(IJAttempter &attempter)
+        : attempter(attempter)
+    {
+
+    }
+
+private:
+    IJAttempter &attempter;
+};
+
+@endif
+@if '%{UiClassBase}' === 'QObject'
+%{UiClassName}::%{UiClassName}(IJAttempter &attempter, QObject *parent)
+    : QObject(parent)
+@if '%{IncludeQSharedData}'
+    , data(new %{UiClassName}Data(attempter)
+@else
+    , q_attempter(attempter)
+@endif
+@elsif '%{UiClassBase}' === 'QWidget' || '%{UiClassBase}' === 'QDialog' || '%{UiClassBase}' === 'QMainWindow'
+%{UiClassName}::%{UiClassName}(IJAttempter &attempter, QWidget *parent)
+    : %{UiClassBase}(parent)
+@if '%{IncludeQSharedData}'
+    , data(new %{UiClassName}Data(attempter)
+@else
+    , q_attempter(attempter)
+@endif
+@else
+%{UiClassName}::%{UiClassName}(IJAttempter &attempter)
+@if '%{IncludeQSharedData}'
+    : data(new %{UiClassName}Data(attempter)
+@else
+    : q_attempter(attempter)
+@endif
+@endif
+{
+
+}
+
+%{UiClassName}::~%{UiClassName}()
+{
+
+}
+
+@if '%{IncludeQSharedData}'
+
+%{UiClassName}::%{UiClassName}(const %{UiClassName} &rhs) : data(rhs.data)
+{
+
+}
+
+%{UiClassName} &%{UiClassName}::operator=(const %{UiClassName} &rhs)
+{
+    if (this != &rhs) {
+        data.operator=(rhs.data);
+    }
+
+    return *this;
+}
+
+%{UiClassName}::~%{UiClassName}()
+{
+
+}
+@endif
+%{JS: Cpp.closeNamespaces('%{UiClass}')}\
